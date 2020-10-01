63596b6ca824fb0870e99f4ff921489433d439e3
Ilya <51325702+treger78@users.noreply.github.com>
2020-10-01 21:32:53 +0300

    feat: make decode func

--- src/index.js
+++ src/index.js
@@ -38,7 +38,43 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+
+    let keyArr = Object.keys(MORSE_TABLE);
+    let oldKey = Object.keys(MORSE_TABLE);
+
+    keyArr = JSON.stringify(keyArr);   
+    keyArr = keyArr.replace(/\"/g, '').replace(/\./g, '10').replace(/\-/g, '11').replace('[', '').replace(']', '');
+    keyArr = keyArr.split(',');
+
+    for (let i = 0; i < keyArr.length; i++) {
+      if (keyArr[i].length < 10) {
+        for (let el = keyArr[i].length; el < 10; el++) {
+          keyArr[i] = '0' + keyArr[i];
+        }
+      }
+
+      if (oldKey[i] !== keyArr[i]) {
+          Object.defineProperty(MORSE_TABLE, keyArr[i],
+              Object.getOwnPropertyDescriptor(MORSE_TABLE, oldKey[i]));
+          delete MORSE_TABLE[oldKey[i]];
+      }  
+
+    }
+
+  expr = expr.match(/.{1,10}/g);
+
+  let result = '';
+
+  for (let i = 0; i < expr.length; i++) {
+    if (MORSE_TABLE.hasOwnProperty(expr[i])) {
+      result += MORSE_TABLE[expr[i]];
+    } else if (expr[i] === '**********') {
+      result += ' ';
+    }
+  }
+
+  return result;
+
 }
 
 module.exports = {
